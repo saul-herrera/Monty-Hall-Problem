f6e91e37c154d411ae3169e41d279ceb7e47ec57
Saúl A. Herrera <100383095+saul-herrera@users.noreply.github.com>
2022-03-20 15:29:38 -0600

    Add files via upload

---
+++ Monty_Hall.py
@@ -0,0 +1,38 @@
+"""
+Created on Sat Mar 12 17:53:51 2022 - https://github.com/saul-herrera
+
+"""
+import random as rm
+
+Doors = ["Goat", "Goat", "Car"]
+player_switches = True
+N_trials = 10000
+win_counter = 0.
+lose_counter = 0.
+for i in range(N_trials):
+    #All doors closed:
+    unpicked_doors = Doors.copy()
+   ## print(unpicked_doors)
+    #Player picks door at random:
+    players_pick_number = rm.choice(range(len(Doors)))
+    players_pick = Doors[players_pick_number]
+    unpicked_doors.pop(players_pick_number)    
+    #Host reveals a goat:
+    unpicked_doors.remove("Goat")   
+    #Host offers door change:
+    if player_switches:
+            players_pick = unpicked_doors[0]
+    if players_pick in ["Goat"]:
+        lose_counter += 1
+    elif players_pick in ["Car"]:
+        win_counter += 1
+    else: 
+        print("There's some error")
+print("Player switches doors -> " + str(player_switches))
+print("Outcome after playing " + str(N_trials) + " times:\n" 
+      "% of wins = " + str(100*win_counter/N_trials)+"%" 
+      + "\n% of loses = " + str(100*lose_counter/N_trials)+"%")
+
+    
+        
+            
